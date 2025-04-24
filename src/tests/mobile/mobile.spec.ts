a9ab0ff12038a2e2781168cdf75d42d2a150a976
AzeezAAlimi <azeezaalimi@gmail.com>
2025-04-24 16:57:29 +0200

    new test

---
+++ src/tests/mobile/mobile.spec.ts
@@ -0,0 +1,26 @@
+import { test } from '../../fixtures/fixtures';
+import { devices } from '@playwright/test';
+import { userData } from '../../config/testData';
+
+const iphone15ProMax = devices['iPhone 15 Pro Max'];
+
+test.use({ ...iphone15ProMax });
+
+test.describe('Smoke: Login flow', () => {
+  test.beforeEach(async ({ homePage, page }) => {
+    await homePage.goTo();
+    await page.screenshot({ path: 'src/tests/mobile/screenshot/iphone15ProMax.png', fullPage: true})
+  });
+
+  test('The standard_user should be able to log in', async ({ homePage }) => {
+    const { userName, password } = userData.standardUser;
+    await homePage.standardlogin(userName, password);
+  });
+
+  test('The locked_out_user should not be able to log in', async ({
+    homePage,
+  }) => {
+    const { userName, password } = userData.lockedUser;
+    await homePage.lockedOutUserlogin(userName, password);
+  });
+});
